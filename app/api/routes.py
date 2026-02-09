ae891c377eb5b4e361a173b68fdf57d3ce43b80a
plon-Susk7 <priyashshah4@gmail.com>
2026-02-09 22:09:54 +0530

    setting project for demo ready

--- app/api/routes.py
+++ app/api/routes.py
@@ -12,33 +12,38 @@ client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 def read_root():
     return {"Hello": "World"}
 
-@router.get("/available_bsd")
-def get_available_bsd():
-    list_bsd = os.listdir("app/bsd")
-    return {"available_bsd": list_bsd}
+# @router.get("/available_bsd")
+# def get_available_bsd():
+#     list_bsd = os.listdir("app/bsd")
+#     return {"available_bsd": list_bsd}
 
-@router.get("/get_stories/{bsd_name}")
-def get_stories(bsd_name: str):
-    bsd_path = f"app/bsd/{bsd_name}"
-    if not os.path.exists(bsd_path):
-        return {"error": "BSD not found"}
-    stories = os.listdir(bsd_path)
-    return {"stories": stories}
+# @router.get("/get_stories/{bsd_name}")
+# def get_stories(bsd_name: str):
+#     bsd_path = f"app/bsd/{bsd_name}"
+#     if not os.path.exists(bsd_path):
+#         return {"error": "BSD not found"}
+#     stories = os.listdir(bsd_path)
+#     return {"stories": stories}
 
-@router.get("/get_story_content/{bsd_name}/{story_name}")
-def get_story_content(bsd_name: str, story_name: str):
-    story_path = f"app/bsd/{bsd_name}/{story_name}"
+@router.get("/api/chat/start/{project_uri}")
+def get_project_context_path(project_uri: str):
+    return {"context_path": f"app/bsd/{project_uri}/US1 - NMPA Product Registration & Compliance System (PRCS)"}
+
+
+@router.get("/get_story_content/{context_path}")
+def get_story_content(context_path: str):
+    story_path = context_path
     if not os.path.exists(story_path):
         return {"error": "Story not found"}
     with open(story_path, "r") as f:
         content = f.read()
     return {"content": content}
 
-@router.websocket("/ws/chat/{bsd_name}/{story_name}")
-async def model_conversation(websocket : WebSocket, bsd_name: str, story_name: str):
+@router.websocket("/ws/api/chat/{context_path:path}")
+async def model_conversation(websocket : WebSocket, context_path: str):
 
     await websocket.accept()
-    pre_context_bsd = get_story_content(bsd_name, story_name)
+    pre_context_bsd = get_story_content(context_path)
     chat = ChatGraph(pre_context=pre_context_bsd["content"])
     try:
         while True:
@@ -48,3 +53,5 @@ async def model_conversation(websocket : WebSocket, bsd_name: str, story_name: s
             await websocket.send_text(message.content)
     except WebSocketDisconnect:
         print("Client disconnected")
+
+
--- app/main.py
+++ app/main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 from app.api.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # For hackathon/dev only
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
