2476685201c1682e6f97f02c592b7fb806ce820c
plon-Susk7 <priyashshah4@gmail.com>
2026-02-09 19:47:00 +0530

    added few bsd examples and 2 endpoints

--- app/api/routes.py
+++ app/api/routes.py
@@ -12,6 +12,19 @@ client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 def read_root():
     return {"Hello": "World"}
 
+@router.get("/available_bsd")
+def get_available_bsd():
+    list_bsd = os.listdir("app/bsd")
+    return {"available_bsd": list_bsd}
+
+@router.get("/get_stories/{bsd_name}")
+def get_stories(bsd_name: str):
+    bsd_path = f"app/bsd/{bsd_name}"
+    if not os.path.exists(bsd_path):
+        return {"error": "BSD not found"}
+    stories = os.listdir(bsd_path)
+    return {"stories": stories}
+
 @router.websocket("/ws/chat")
 async def model_conversation(websocket : WebSocket):
 
